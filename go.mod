d202fa6e2128868e4a3b126a06506ebcc79a8eef
Abhijeet Muneshwar <OPENINGKNOTS@GMAIL.COM>
2024-12-12 10:45:01 +0530

    End-of-data signaling
    No signal for end of data

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ABHIJEET-MUNESHWAR/Concurrent-Talkers
+
+go 1.23
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+
+func main() {
+	str := "one,two,,four"
+	in := make(chan string)
+	go func() {
+		words := strings.Split(str, ",")
+		for _, word := range words {
+			in <- word
+		}
+	}()
+	for {
+		word := <-in
+		if word != "" {
+			fmt.Printf("%s ", word)
+		}
+	}
+}
